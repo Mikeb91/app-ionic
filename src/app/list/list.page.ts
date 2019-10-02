e4a29248df230a87d8c7d6374a545c3f1c7de01e
Mike Ballesteros <mike_b91@hotmail.com>
2019-10-01 21:03:44 -0500

    Añadiendo primeras caracteristicas ionic

---
+++ src/app/list/list.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: 'list.page.html',
+  styleUrls: ['list.page.scss']
+})
+export class ListPage implements OnInit {
+  private selectedItem: any;
+  private icons = [
+    'ios-sunny',
+    'logo-angular',
+    'logo-apple',
+    'logo-css3',
+    'logo-facebook',
+    'logo-javascript',
+    'logo-nodejs',
+    'logo-sass',
+    'bluetooth',
+    'logo-steam'
+  ];
+  public items: Array<{ title: string; note: string; icon: string }> = [];
+  constructor() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[i-1]
+      });
+    }
+  }
+
+  ngOnInit() {
+  }
+  // add back when alpha.4 is out
+  // navigate(item) {
+  //   this.router.navigate(['/list', JSON.stringify(item)]);
+  // }
+}
